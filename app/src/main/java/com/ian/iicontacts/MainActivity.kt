df0c1e6e328815cd326747a7cf87e9667ef27ede
Iman Irandoost <iirandoost@eclassifiedsgroup.com>
2022-03-19 10:38:53 +0100

    Bring orange colors

--- app/src/main/java/com/ian/iicontacts/MainActivity.kt
+++ app/src/main/java/com/ian/iicontacts/MainActivity.kt
@@ -3,28 +3,32 @@ package com.ian.iicontacts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.ian.iicontacts.ui.theme.IIContactsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            IIContactsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContent { MainContent() }
+    }
+}
+
+@Composable
+fun MainContent(darkMode: Boolean = isSystemInDarkTheme()) {
+    IIContactsTheme(darkTheme = darkMode) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Greeting("Android")
         }
     }
 }
@@ -34,10 +38,14 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.PIXEL_4, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    IIContactsTheme {
-        Greeting("Android")
-    }
+    MainContent(true)
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4, showSystemUi = true)
+@Composable
+fun LightPreview() {
+    MainContent(false)
 }
--- app/src/main/java/com/ian/iicontacts/ui/theme/Color.kt
+++ app/src/main/java/com/ian/iicontacts/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.ian.iicontacts.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Orange200 = Color(0xFFFFC947)
+val Orange500 = Color(0xFFFF9800)
+val Orange700 = Color(0xFFF57C00)
