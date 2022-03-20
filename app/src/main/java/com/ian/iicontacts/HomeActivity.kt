fe7faafb4c56fd6a5d90cc57eaf6f7ab3a4b86d7
Iman Irandoost <iirandoost@eclassifiedsgroup.com>
2022-03-20 09:47:06 +0100

    Add contacts permission

--- app/src/main/java/com/ian/iicontacts/HomeActivity.kt
+++ app/src/main/java/com/ian/iicontacts/HomeActivity.kt
@@ -1,18 +1,70 @@
 package com.ian.iicontacts
 
-import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.*
+import androidx.core.content.ContextCompat
 import com.ian.iicontacts.screens.home.HomeScreen
 import com.ian.iicontacts.ui.theme.IIContactsTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { isGranted ->
+            if (isGranted.all { it.value }) {
+                setContent { MainContent() }
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // features requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MainContent() }
+        requestContactsPermission()
+    }
+
+    private fun requestContactsPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_CONTACTS
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    setContent { MainContent() }
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.WRITE_CONTACTS) ||
+                        shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
+                    // In an educational UI, explain to the user why your app requires this
+                    // permission for a specific feature to behave as expected. In this UI,
+                    // include a "cancel" or "no thanks" button that allows the user to
+                    // continue using your app without granting the permission.
+                    //            showInContextUI(...)
+                }
+                else -> {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    requestPermissionLauncher.launch(
+                        arrayOf(
+                            Manifest.permission.READ_CONTACTS,
+                            Manifest.permission.WRITE_CONTACTS,
+                        ),
+                    )
+                }
+            }
+        }
     }
 }
 
